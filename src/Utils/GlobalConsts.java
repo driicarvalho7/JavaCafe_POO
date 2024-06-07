9d910651fa1ddff14c1bef81bd0bdee01a810ece
Adriano Carvalho <adrianocarvalho@MacBook-Pro-de-Adriano.local>
2024-06-07 16:47:39 -0300

    corrigindo GlobalConsts.java

--- src/Utils/GlobalConsts.java
+++ src/Utils/GlobalConsts.java
@@ -1,7 +1,15 @@
 package Utils;
 
+// Se você estiver usando o S.O do Windows, as barras tem que ser no formato '\\'
+// Se você estiver usando o S.O do MacOS, as barras tem que ser no formato '/'
 public class GlobalConsts {
-    private static final String _PATH = "C:\\Users\\adria\\IdeaProjects\\JavaCafe\\";
-    public static final String INVENTARIO_FILE = _PATH + "src\\db_system_pos\\inventario.txt";
-    public static final String PEDIDOS_FILE = _PATH + "src\\db_system_pos\\pedidos.txt";
+    // Minhas configurações no Windows
+    //private static final String _PATH = "C:\\Users\\adria\\IdeaProjects\\JavaCafe\\";
+    //public static final String INVENTARIO_FILE = _PATH + "src\\db_system_pos\\inventario.txt";
+    //public static final String PEDIDOS_FILE = _PATH + "src\\db_system_pos\\pedidos.txt";
+    
+    // Minhas configurações no Mac
+    private static final String _PATH = "/Users/adrianocarvalho/IdeaProjects/JavaCafe_POO/";
+    public static final String INVENTARIO_FILE = _PATH + "src/db_system_pos/inventario.txt";
+    public static final String PEDIDOS_FILE = _PATH + "src/db_system_pos/pedidos.txt";
 }
